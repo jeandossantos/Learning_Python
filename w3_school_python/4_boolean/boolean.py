66d25b12771348fc2ae8f41417c831222c9e2560
Jean F. dos Santos <jeanddg@hotmail.com>
2023-06-21 19:49:19 -0300

    boolean

---
+++ w3_school_python/4_boolean/boolean.py
@@ -0,0 +1,14 @@
+print(bool("abc"))  # => True
+print(bool(123))  # => True
+print(bool(["apple", "cherry", "banana"]))  # => True
+
+print("##############FALSY##################")
+
+
+print(bool(False))  # => False
+print(bool(None))  # => False
+print(bool(0))  # => False
+print(bool(""))  # => False
+print(bool(()))  # => False
+print(bool([]))  # => False
+print(bool({}))  # => False
