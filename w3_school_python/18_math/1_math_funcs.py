3329d5f88616e9eedaa84a22be85bc7547eaabf7
Jean F. dos Santos <jeanddg@hotmail.com>
2023-06-24 17:08:51 -0300

    math

---
+++ w3_school_python/18_math/1_math_funcs.py
@@ -0,0 +1,19 @@
+x = min(5, 10, 25) # retorna menor números entre números informados
+y = max(5, 10, 25) # retorna maior números entre números informados
+
+print(x) # 5
+print(y) # 25
+
+print("\n--------------------------------")
+
+x = abs(-7.25) # retorna o absoluto do número informado sempre positivo
+
+print(x) # 7.25 
+
+print("\n--------------------------------")
+
+x = pow(4, 3) # 4 elevado a 3 => 4 x 4 x 4 = 64
+
+print(x) # 64
+
+print("\n--------------------------------")
---
+++ w3_school_python/18_math/2_math_module.py
@@ -0,0 +1,13 @@
+import math
+
+x = math.sqrt(64)
+print(x)
+print("\n--------------------------------")
+
+print(math.ceil(1.5)) # math.ceil arredonda para cima
+print(math.floor(1.5)) # math.floor arredonda para baixo
+
+print("\n--------------------------------")
+
+pi = math.pi
+print(pi) # 3.141592653589793
